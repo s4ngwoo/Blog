0c6ad333117139ab573b9519f72ea828da023689
Lee Sang Woo <s4ngwoo.lee@gmail.com>
2023-05-01 21:29:31 +0900

    add test for post_detail and post_list.

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, PostCategory, PostSeries, PostTag
 
 # Create your tests here.
 
@@ -35,11 +35,13 @@ class TestView(TestCase):
                 title='first post.',
                 subtitle='subtitle of first post',
                 content = 'hello world!',
+                branch = 1,
                 )
         post_002 = Post.objects.create(
                 title='second post.',
                 subtitle='subtitle of second post',
                 content = 'there are second all the time.',
+                branch = 1,
                 )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -53,3 +55,58 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         # 3.4 '아직 게시물이 없습니다.'라는 문구는 더이상 보이지 않는다.
         self.assertNotIn("No Post.", main_area.text)
+
+    def test_post_detail(self):
+        # 1.1 포스트가 하나 있다.
+        category_001 = PostCategory.objects.create(
+                title = 'cat',
+                )
+        series_001 = PostSeries.objects.create(
+                title = 'se',
+                )
+        tag_001 = PostTag.objects.create(
+                title = 'ta',
+                )
+        post_001 = Post.objects.create(
+                title = 'first post.',
+                subtitle = 'subtitle of first post.',
+                content = 'content of first post.',
+                branch = 1,
+                category = category_001,
+                series = series_001,
+                tag = tag_001,
+                )
+        # 1.2 그 포스트의 url은 '/1/' 이다.
+        self.assertEqual(post_001.get_absolute_url(), '/1/')
+
+        # 2. 첫 번째 포스트이 상세 페이지 테스트
+        # 2.1 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
+        navbar = soup.nav
+        self.assertIn('Home', navbar.text)
+        self.assertIn('About', navbar.text)
+        self.assertIn('Archive', navbar.text)
+        self.assertIn('Contact', navbar.text)
+
+        # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
+        self.assertIn(post_001.title, soup.title.text)
+
+        # 2.4 첫 번째 포스트의 제목이 헤더의 타이틀 영역에 있다.
+        header_area = soup.find('div', id='header-area')
+        title_area = soup.find('div', id='title-area')
+        self.assertIn(post_001.title, title_area.text)
+        # 2.5 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
+        post_area = soup.find('div', id='post-area')
+        self.assertIn(post_001.content, post_area.text)
+        # 2.6 첫 번째 포스트의 카테고리가 포스트 영역에 있다.
+        self.assertIn(str(post_001.category), post_area.text)
+        # 2.7 첫 번째 포스트의 시리즈가 포스트 영역에 있다.
+        self.assertIn(str(post_001.series), post_area.text)
+        # 2.8 첫 번째 포스트의 태그가 포스트 영역에 있다.
+        self.assertIn(str(post_001.tag), post_area.text)
+
+
